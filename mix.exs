a254a56e5be28219361abf1b1ad0f5bd7fe89699
Alex Filatov <alex@alexfilatov.com>
2022-01-26 23:29:28 +0000

    try to lower elixir to 1.9

--- mix.exs
+++ mix.exs
@@ -5,7 +5,7 @@ defmodule Borsh.MixProject do
     [
       app: :borsh,
       version: "0.1.0",
-      elixir: "~> 1.12",
+      elixir: "~> 1.9",
       start_permanent: Mix.env() == :prod,
       package: package(),
       description: description(),
